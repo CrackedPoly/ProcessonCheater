84a82c9974a49f2a08d77eedfe72100df578b696
UnknwoonUser <1092003187@qq.com>
2021-07-22 15:22:11 +0800

    update

--- ProcessonCheater.py
+++ ProcessonCheater.py
@@ -14,25 +14,6 @@ class User:
         "addLink": "https://www.processon.com/view/addlink"
     }
 
-    def expand(self):
-        """
-        扩容一次
-        """
-        # 创建流程图
-        result = currentUser.createFlow()
-        searchObj = re.search(r'var chartId = "([a-zA-Z0-9]*)"', result.text)
-        # 找到图表的ID
-        chartId: str = searchObj.group(1)
-        # 自己给自己点赞
-        self.getSharedLink(chartId)
-        for index in range(5):
-            self.dolike(chartId)
-        # 最大容量
-        maxChart = self.getMaxChart()
-        print("扩容一次完成,当前最大容量:%d" % maxChart)
-        # 删除新建的图表
-        currentUser.deleteChart(chartId)
-
     def login(self):
         """
         登录
@@ -51,49 +32,17 @@ class User:
         else:
             print("用户 %s 登录成功" % self.login_email)
 
-    def createFlow(self):
-        """
-        创建流程图
-        """
-        return self.s.get(User.API['createFlow'])
-
-    def getSharedLink(self, chartId):
-        """
-        获得分享后的链接
-        """
-        result = self.s.post(self.API['addLink'], data={'chartId': chartId})
-        # 测试用
-        # data = result.json()
-        # return data['viewLinkId']
-
-    def dolike(self, chartId):
+    def doLike(self, link):
         """
         点赞
-        :param chartId:
+        :param link:
         """
-        r = self.s.post(User.API['doLike'], data={'chartId': chartId})
-
-    def getMaxChart(self):
-        """
-        获取最大容量
-        """
-        result = self.s.get(User.API['MaxChart'])
-        resultObj = json.loads(result.text)
-        return resultObj['totalcount']
-
-    def deleteChart(self, chartId):
-        """
-        完全删除指定图表
-        """
-        postData = {
-            "type": "delete",
-            "fileIds": chartId,
-            "targetFolderId": ""
-        }
-        # 把创建的空流程图移到回收站
-        self.s.post(User.API['mvTrash'], data=postData)
-        # 清空回收站
-        self.s.post(User.API['clearTrash'], data={'fileType': 'all'})
+        result = self.s.get(link)
+        searchObj = re.search(r'var chartId = "([a-zA-Z0-9]*)"', result.text)
+        # 找到图表的ID
+        chartId: str = searchObj.group(1)
+        # 点赞
+        self.s.post(User.API['doLike'], data={'chartId': chartId})
 
     def __init__(self, login_email, login_password):
         self.s = requests.Session()
@@ -103,23 +52,9 @@ class User:
 
 
 if __name__ == "__main__":
-
-    # 获取必要信息
-    login_email = input("请输入您的账号:")
-    login_password = input("请输入您的密码:")
-
-    print("开始初始化...")
-    currentUser = User(login_email, login_password)
-    # 开始扩容
-    print("开始扩容...")
-    # 扩容的数目
-    while True:
-        times = input('请输入扩容数目(纯数字)：')
-        if times.isnumeric():
-            for i in range(eval(times)):
-                currentUser.expand()
-            break
-        else:
-            continue
-
-pass
+    share_link = input("请输入分享链接:")
+    with open("users.json.bak", "r", encoding="utf8") as fp:
+        json_data = json.load(fp)
+        for user in json_data:
+            User(user["username"], user["password"]).doLike(share_link)
+    print("点赞完成")
